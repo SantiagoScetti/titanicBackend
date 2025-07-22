21c6cebae9500a68c6bbfbe052f527b95c7d7c7a
Santiago Scetti <santiscetti22@gmail.com>
2025-07-22 19:03:16 -0300

    Corregir URL de API en Home.jsx

--- app/main.py
+++ app/main.py
@@ -30,6 +30,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_origins=["https://titanic-frontend.netlify.app/"],  # Reemplazá con la URL real de Netlify
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
